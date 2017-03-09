bbd5ea22d5e8628e3eba9015796928bcc097f249
KID <nakid001@gmail.com>
2017-03-10 00:51:50 +0700

    Complete merge model1

--- Assets/Script/WeaponNameController.cs
+++ Assets/Script/WeaponNameController.cs
@@ -8,6 +8,8 @@ public class WeaponNameController : MonoBehaviour
     public GameObject Pistol;
 	public static string weaponname="hand";
 	bool fix =true;
+    bool parented=false;
+    GameObject myNew;
     // Use this for initialization
     void Start()
     {
@@ -15,13 +17,27 @@ public class WeaponNameController : MonoBehaviour
     }
 	void Update()
 	{
-		if (weaponname == "pistol" && fix) {
-			var myNew = Instantiate (Pistol, gameObject.transform.position, gameObject.transform.rotation);
+
+		if (weaponname == "pistol" && fix || weaponname == "pistol(Clone)"&&fix)
+        {
+            Debug.Log("Add Weapon" + weaponname);
+            myNew = Instantiate (Pistol, gameObject.transform.position, gameObject.transform.rotation);
 			myNew.transform.parent = gameObject.transform;
             Cursor.visible = false;
 			fix = false;
+            parented = true;
 		}
-		gameObject.name = weaponname;
+        if(weaponname=="pistol" && Input.GetKeyDown(KeyCode.G)|| weaponname == "pistol(Clone)" && Input.GetKeyDown(KeyCode.G))
+        {
+            Debug.Log("DropWeapon");
+            weaponname = "hand";
+            Cursor.visible = true;
+            parented = false;
+            myNew.transform.parent = null;
+            fix = true;
+        }
+
+        gameObject.name = weaponname;
 	}
 
     // Update is called once per frame

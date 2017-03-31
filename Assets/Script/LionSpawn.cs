5ceaccd2cbeaf70cae2de8a3fb0c2a1c1d73fc6d
kid <nakid001@gmail.com>
2017-03-31 14:15:21 +0700

    fuck

---
+++ Assets/Script/LionSpawn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LionSpawn : MonoBehaviour {
+
+    public GameObject LionModel;
+    public float AllTime = 0;
+    public static int number = 0;
+    public static bool start = true;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AllTime += Time.deltaTime;
+        // Debug.Log("number = " +number);
+        //Debug.Log(GameObject.FindGameObjectsWithTag("PigTag").Length);
+
+        if (GameObject.FindGameObjectsWithTag("LionTag").Length < number && start)
+        {
+            Vector3 position = new Vector3(Random.Range(-7f, 8.5f), 5, Random.Range(0, 10f));
+            var myNew = Instantiate(LionModel, position, Quaternion.identity);
+            myNew.transform.parent = gameObject.transform;
+        }
+        else
+        {
+            LionSpawn.start = false;
+        }
+
+
+    }
+}
---
+++ Assets/Script/SpawnUnitStep.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnUnitStep : MonoBehaviour {
+
+    int [] maxpig = {10,7,6,5,2,0,0};
+    int [] maxtiger = { 0, 2, 3, 5, 7,0,0 };
+    int stage = 0;
+    void start()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameObject.FindGameObjectsWithTag("PigTag").Length==0)
+        {
+            Debug.Log("stage= " + stage);
+            PigSpawn.number = maxpig[stage];
+            LionSpawn.number = maxtiger[stage];
+            PigSpawn.start = true;
+            LionSpawn.start = true;
+            stage++;
+        }
+
+
+
+    }
+}

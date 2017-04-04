8d62a05eac85cbeed3f93855e14155e433bc2be7
booranasit <booranasit@booranasits-MacBook.local>
2017-04-04 23:38:29 +0700

    pause menu already active

---
+++ Assets/pause_menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pause_menu : MonoBehaviour {
+	
+	public void Resume()
+	{
+		GameObject.Find("PauseMenu").GetComponent<Canvas>().enabled = false;
+	}
+
+	public void Restart()
+	{
+		SceneManager.LoadScene("MyGame");
+	}
+
+	public void ShowKeyControl()
+	{
+		GameObject.Find("KeyboardControl").GetComponent<Canvas>().enabled = true;
+	}
+
+	public void Back()
+	{
+		GameObject.Find("KeyboardControl").GetComponent<Canvas>().enabled = false;
+	}
+
+
+
+
+}
